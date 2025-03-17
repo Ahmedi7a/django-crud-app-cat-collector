7d9566874fe2007155eaddf593fd4058b92f37b3
Ahmedi7a <ahmedamralla73@email.com>
2025-03-17 13:24:58 +0300

    added model, db, pages,routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+# controller
 
-# Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Cat
+
+# # fake data to check
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
+
+# # Create a list of Cat instances
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
 
 # Define the home view function
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
